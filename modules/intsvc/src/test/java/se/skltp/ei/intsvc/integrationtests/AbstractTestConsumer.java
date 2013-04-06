87249cb59fe0e46a03129e1a306a847268e0732a
magnus.larsson@callistaenterprise.se <magnus.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2013-04-06 22:04:30 +0000

    EI, update-integration-test on its way...

---
+++ modules/intsvc/src/test/java/se/skltp/ei/intsvc/integrationtests/AbstractTestConsumer.java
@@ -0,0 +1,52 @@
+package se.skltp.ei.intsvc.integrationtests;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.message.Message;
+
+public abstract class AbstractTestConsumer<ServiceInterface> {
+
+	public static final String SAMPLE_ORIGINAL_CONSUMER_HSAID = "sample-original-consumer-hsaid";
+	
+	protected ServiceInterface _service = null;	
+
+    private Class<ServiceInterface> _serviceType;
+
+    /**
+     * Constructs a test consumer with a web service proxy setup for communication using HTTPS with Mutual Authentication
+     * 
+     * @param serviceType, required to be able to get the generic class at runtime, see http://stackoverflow.com/questions/3403909/get-generic-type-of-class-at-runtime
+     * @param serviceAddress
+     */
+	public AbstractTestConsumer(Class<ServiceInterface> serviceType, String serviceAddress) {
+
+		_serviceType = serviceType;
+		
+		JaxWsProxyFactoryBean proxyFactory = new JaxWsProxyFactoryBean();
+		proxyFactory.setServiceClass(getServiceType());
+		proxyFactory.setAddress(serviceAddress);
+
+		// Used for HTTPS
+        /*
+		SpringBusFactory bf = new SpringBusFactory();
+		URL cxfConfig = this.getClass().getClassLoader().getResource("agp-cxf-test-consumer-config.xml");
+		if (cxfConfig != null) {
+			proxyFactory.setBus(bf.createBus(cxfConfig));
+		}
+        */
+
+		_service = proxyFactory.create(getServiceType()); 
+	}
+
+    Class<ServiceInterface> getServiceType() {
+    	return _serviceType;
+    }
+}
