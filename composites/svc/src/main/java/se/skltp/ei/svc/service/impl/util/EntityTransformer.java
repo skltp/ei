ee05936fa27c848bc3e6dcd52fc353e20dea5fc0
peter.larsson@callistaenterprise.se <peter.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2013-04-15 06:47:30 +0000

    EI: Added date formatting and parsing functions.

--- composites/svc/src/main/java/se/skltp/ei/svc/service/impl/util/EntityTransformer.java
+++ composites/svc/src/main/java/se/skltp/ei/svc/service/impl/util/EntityTransformer.java
@@ -1,10 +1,22 @@
 package se.skltp.ei.svc.service.impl.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import riv.itintegration.engagementindex._1.EngagementType;
 import se.skltp.ei.svc.entity.model.Engagement;
 
 public class EntityTransformer {
-
+	
+	static SimpleDateFormat dateFormatter;
+	
+	static {
+		dateFormatter = new SimpleDateFormat("yyyyMMddHHmmss");
+		// strict syntax mode
+		dateFormatter.setLenient(false);
+	}
+	
 	/**
 	 * Transform an engagement from the service model to the entity model
 	 * 
@@ -23,9 +35,10 @@ public class EntityTransformer {
 				eIn.getSourceSystem(),
 				eIn.getOwner(),
 				eIn.getClinicalProcessInterestId());
-				
-// FIXME	    eOut.setCreationTime(eIn.getCreationTime());
-// FIXME	    eOut.setUpdateTime(eIn.getUpdateTime());
+		
+		eOut.setCreationTime(parseDate(eIn.getCreationTime()));
+		eOut.setUpdateTime(parseDate(eIn.getUpdateTime()));
+		eOut.setMostRecentContent(parseDate(eIn.getMostRecentContent()));
 		
 	    return eOut;
 	}
@@ -46,12 +59,38 @@ public class EntityTransformer {
 	    eOut.setLogicalAddress(key.getLogicalAddress());
 	    eOut.setBusinessObjectInstanceIdentifier(key.getBusinessObjectInstanceIdentifier());
 	    eOut.setSourceSystem(key.getSourceSystem());
-// FIXME	    eOut.setCreationTime(eIn.getCreationTime());
 	    eOut.setOwner(key.getOwner());
-// FIXME	    eOut.setUpdateTime(eIn.getUpdateTime());
 	    eOut.setClinicalProcessInterestId(key.getClinicalProcessInterestId());
+
+	    eOut.setCreationTime(forrmatDate(eIn.getCreationTime()));
+	    eOut.setMostRecentContent(forrmatDate(eIn.getMostRecentContent()));	    
+	    eOut.setUpdateTime(forrmatDate(eIn.getUpdateTime()));
 		
 	    return eOut;
+	}
+	
+	/**
+	 * Returns a formatted date according to the service contract format.
+	 * 
+	 * @param date the date to format.
+	 * @return the formatted date.
+	 */
+	public static String forrmatDate(Date date) {
+		return (date == null) ? null : dateFormatter.format(date);
+	}
+	
+	/**
+	 * Returns a date.
+	 * 
+	 * @param date the date in text.
+	 * @return the date.
+	 */
+	public static Date parseDate(String date) {
+		try {
+			return (date == null) ? null : dateFormatter.parse(date);
+		} catch (ParseException e) {
+			throw new IllegalArgumentException("Imput date is not according to expected format \"YYYYMMDDhhmmss\": " + date, e);
+		}
 	}
 
 }
---
+++ composites/svc/src/test/java/se/skltp/ei/svc/service/impl/util/EntityTransformerTest.java
@@ -0,0 +1,34 @@
+package se.skltp.ei.svc.service.impl.util;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class EntityTransformerTest {
+
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	public void dateParseTest() {		
+		final String sDate = "19611028130123";
+		
+		Date date = EntityTransformer.parseDate(sDate);
+		
+		String fDate = EntityTransformer.forrmatDate(date);
+		
+		assertEquals(sDate, fDate);
+	}
+	
+	@Test
+	public void incorrectDateParseTest() {
+		exception.expect(IllegalArgumentException.class);
+		final String sDate = "1961-10-28 13:01:23";
+		Date date = EntityTransformer.parseDate(sDate);
+		System.out.println(date);
+	}
+}
