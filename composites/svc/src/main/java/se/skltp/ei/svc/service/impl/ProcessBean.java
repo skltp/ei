34ccce19775827b2484dd98416780a8d80d294f1
peter.larsson@callistaenterprise.se <peter.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2013-07-31 15:19:15 +0000

    EI:  More refactoring of valdiation code, use the same structure regardless of initiator (update, process notification). All validations are now carried out in 1 pass (loop) instead of 3.

--- composites/svc/src/main/java/se/skltp/ei/svc/service/impl/ProcessBean.java
+++ composites/svc/src/main/java/se/skltp/ei/svc/service/impl/ProcessBean.java
@@ -23,7 +23,6 @@ import static se.skltp.ei.svc.service.api.EiErrorCodeEnum.EI002_DUPLICATE_UPDATE
 import static se.skltp.ei.svc.service.api.EiErrorCodeEnum.EI003_LOGICALADDRESS_DONT_MATCH_OWNER;
 import static se.skltp.ei.svc.service.impl.util.EntityTransformer.toEntity;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -55,12 +54,12 @@ public class ProcessBean implements ProcessInterface {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProcessBean.class);
 
-	private String owner;
-	
-	// Maximum number of engagements that should be processed
-	public static int MAX_NUMBER_OF_ENGAGEMENTS = 1000;
-	 
-	
+    private String owner;
+
+    // Maximum number of engagements that should be processed
+    public static int MAX_NUMBER_OF_ENGAGEMENTS = 1000;
+
+
     private EngagementRepository engagementRepository;
 
     private static UpdateResponseType RESPONSE_OK = new UpdateResponseType() {
@@ -69,19 +68,19 @@ public class ProcessBean implements ProcessInterface {
             return ResultCodeEnum.OK;
         }
     };
-    
+
     private static ProcessNotificationResponseType NOTIFICATION_RESPONSE_OK = new ProcessNotificationResponseType() {
         @Override
         public ResultCodeEnum getResultCode() {
             return ResultCodeEnum.OK;
         }
     };
-    
-    
+
+
     public void setOwner(String owner) {
-		this.owner = owner;
-	}
-    
+        this.owner = owner;
+    }
+
     @Autowired
     public void setEngagementRepository(EngagementRepository engagementRepository) {
         LOG.info("ProcessBean got its engagementRepository injected");
@@ -96,12 +95,34 @@ public class ProcessBean implements ProcessInterface {
      */
     @Override 
     public void validateUpdate(Header header, UpdateType request) {
-    	validateLogicalAddress(header);
-        validateUniqueness(request);
-        validateMaxLength(request.getEngagementTransaction());
-        validateRequiredFieldsInEngagements(request.getEngagementTransaction());
+        // R7
+        validateLogicalAddress(header);
+
+        validateEngagementTransactions(request.getEngagementTransaction(), false);
     }
-    
+
+    //
+    private void validateEngagementTransactions(final List<EngagementTransactionType> engagementTransactions, boolean ownerCheck) {
+        validateMaxLength(engagementTransactions);
+
+        final Map<String, Integer> hashCodes = new HashMap<String, Integer>(engagementTransactions.size());
+        int hashCodeIndex = 0;
+        for (final EngagementTransactionType engagementTransaction : engagementTransactions) {
+            final EngagementType et = engagementTransaction.getEngagement();
+
+            // R1 uniqueness
+            final Engagement e = toEntity(et);
+            final Integer otherIndex = hashCodes.put(e.getId(), ++hashCodeIndex);
+            if (otherIndex != null) {
+                throw new EiException(EI002_DUPLICATE_UPDATE_ENTRIES, otherIndex, hashCodeIndex);
+            }
+
+            // mandatory fields
+            validateMandatoryFields(et, ownerCheck);
+
+        }
+
+    }
 
     /**
      * Checks that a mandatory value exists.
@@ -114,69 +135,51 @@ public class ProcessBean implements ProcessInterface {
             throw new EiException(EiErrorCodeEnum.EI004_VALIDATION_ERROR, "mandatory field \"" + name + "\" is missing");            
         }
     }
-    
-    /**
-     * Validates that all required attributes for engagements are supplied 
-     * @param engagementTransactions the transactions
-     * @throws EIException when an mandatory field is not set
-     */
-    private void validateRequiredFieldsInEngagements(List<EngagementTransactionType> engagementTransactions) {
 
-        for (final EngagementTransactionType engagementTransaction : engagementTransactions) {
-            final EngagementType et = engagementTransaction.getEngagement();     	
-            mandatoryValueCheck("registeredResidentIdentification", et.getRegisteredResidentIdentification());        	
-            mandatoryValueCheck("serviceDomain", et.getServiceDomain());
-            mandatoryValueCheck("categorization", et.getCategorization());
-            mandatoryValueCheck("logicalAddress", et.getLogicalAddress());
-            mandatoryValueCheck("businessObjectInstanceIdentifier", et.getBusinessObjectInstanceIdentifier());
-            mandatoryValueCheck("clinicalProcessInterestId", et.getClinicalProcessInterestId());
-            mandatoryValueCheck("sourceSystem", et.getSourceSystem());
-            mandatoryValueCheck("dataController", et.getDataController()); 	
-        }    	
+    /**
+     * Validates all mandatory fields.
+     * 
+     * @param et the engagement record to validate.
+     * @param ownerCheck true if an owner check shall be performed as well, otherwise false.
+     */
+    private static void validateMandatoryFields(final EngagementType et, boolean ownerCheck) {
+        mandatoryValueCheck("registeredResidentIdentification", et.getRegisteredResidentIdentification());          
+        mandatoryValueCheck("serviceDomain", et.getServiceDomain());
+        mandatoryValueCheck("categorization", et.getCategorization());
+        mandatoryValueCheck("logicalAddress", et.getLogicalAddress());
+        mandatoryValueCheck("businessObjectInstanceIdentifier", et.getBusinessObjectInstanceIdentifier());
+        mandatoryValueCheck("clinicalProcessInterestId", et.getClinicalProcessInterestId());
+        mandatoryValueCheck("sourceSystem", et.getSourceSystem());
+        mandatoryValueCheck("dataController", et.getDataController()); 
+        // owner
+        if (ownerCheck) {
+            mandatoryValueCheck("owner", et.getOwner());
+        }
     }
 
-    // Update, R1: Validate uniqueness within the request
-	private void validateUniqueness(UpdateType request) {
-		validateUniqueness(request.getEngagementTransaction());
-	}
-	
-	// R1 - validate uniqueness for engagement in EngangementTransactions
-	private void validateUniqueness(List<EngagementTransactionType> engagementTransactions ) {
-		final Map<String, Integer> hashCodes = new HashMap<String, Integer>(engagementTransactions.size());
-        int hashCodeIndex = 0;
 
-        for (final EngagementTransactionType engagementTransaction : engagementTransactions) {
-            final Engagement e = toEntity(engagementTransaction.getEngagement());
-            final Integer otherIndex = hashCodes.put(e.getId(), ++hashCodeIndex);
-            if (otherIndex != null) {
-            	throw new EiException(EI002_DUPLICATE_UPDATE_ENTRIES, otherIndex, hashCodeIndex);
-            }
+    // Update, R7: Logical address in request equals owner of EI
+    private void validateLogicalAddress(Header header) {
+        if (header == null || header.getReceiverId() == null) {
+            throw new EiException(EI003_LOGICALADDRESS_DONT_MATCH_OWNER,
+                    "missing", owner);
         }
-	}
-	
 
-	// Update, R7: Logical address in request equals owner of EI
-	private void validateLogicalAddress(Header header) {
-		if (header == null || header.getReceiverId() == null) {
-			throw new EiException(EI003_LOGICALADDRESS_DONT_MATCH_OWNER,
-					"missing", owner);
-		}
-    	
-		if (!header.getReceiverId().equals(owner)) {
-			throw new EiException(EI003_LOGICALADDRESS_DONT_MATCH_OWNER,
-					header.getReceiverId(), owner);
-		}
-	}
+        if (!header.getReceiverId().equals(owner)) {
+            throw new EiException(EI003_LOGICALADDRESS_DONT_MATCH_OWNER,
+                    header.getReceiverId(), owner);
+        }
+    }
 
-	// Update/processNotification - max 1000 engagements per request
-	private void validateMaxLength(List<EngagementTransactionType> engagementTransactions ) {
-	
-		if(engagementTransactions.size() > MAX_NUMBER_OF_ENGAGEMENTS) {
-			throw new EiException(EiErrorCodeEnum.EI000_TECHNICAL_ERROR, "The request contains more than " + 
-					MAX_NUMBER_OF_ENGAGEMENTS + " engagements. Maximum number of engagements per request is " + MAX_NUMBER_OF_ENGAGEMENTS + ".");
-		}
-	}
-	
+    // Update/processNotification - max 1000 engagements per request
+    private static void validateMaxLength(List<EngagementTransactionType> engagementTransactions ) {
+
+        if(engagementTransactions.size() > MAX_NUMBER_OF_ENGAGEMENTS) {
+            throw new EiException(EiErrorCodeEnum.EI000_TECHNICAL_ERROR, "The request contains more than " + 
+                    MAX_NUMBER_OF_ENGAGEMENTS + " engagements. Maximum number of engagements per request is " + MAX_NUMBER_OF_ENGAGEMENTS + ".");
+        }
+    }
+
 
     /**
      * Performs an index update transaction. <p>
@@ -193,18 +196,18 @@ public class ProcessBean implements ProcessInterface {
     @Transactional(isolation=Isolation.READ_UNCOMMITTED)
     public UpdateResponseType update(Header header, UpdateType request) {
         LOG.debug("The svc.update service is called");
-          
+
         // Separate deletes from the saves...
         final List<EngagementTransactionType> engagementTransactions = request.getEngagementTransaction();
         final List<Engagement> saveList = new ArrayList<Engagement>(engagementTransactions.size());
         List<Engagement> deleteList = null;
         for (final EngagementTransactionType engagementTransaction : engagementTransactions) {
-        	
+
             EngagementType et = engagementTransaction.getEngagement();
             et.setOwner(this.owner); // According to R6 owner should always be set to owner of the index
-            
+
             final Engagement e = toEntity(et);   
-            
+
             if (engagementTransaction.isDeleteFlag()) {
                 if (deleteList == null) {
                     deleteList = new ArrayList<Engagement>();
@@ -232,142 +235,114 @@ public class ProcessBean implements ProcessInterface {
      * @param header the header
      * @param request the request
      */
-	@Override
-	public void validateProcessNotification(Header header, ProcessNotificationType request) {
-		validateUniqueness(request); // Update R1
-		validateMaxLength(request.getEngagementTransaction());
-		validateRequiredFieldsInEngagements(request.getEngagementTransaction());
-		validateOwner(request);
-	}
-	
-	/**
-	 * R1 - Validate uniqueness within the request
-	 * @param request
-	 * @throws EiException when the validation fail
-	 */
-	private void validateUniqueness(ProcessNotificationType request) {
-		validateUniqueness(request.getEngagementTransaction());
-	}
-	
-	
-	/**
-	 * Validates that the owner is supplied in all engagements
-	 * @param request
-	 */
-	private void validateOwner(ProcessNotificationType request) {
-		
-        for (final EngagementTransactionType engagementTransaction : request.getEngagementTransaction()) {
-        	EngagementType et = engagementTransaction.getEngagement();
-        	if (et.getOwner() == null || et.getOwner().equals("")) {
-        		throw new EiException(EiErrorCodeEnum.EI004_VALIDATION_ERROR, "owner is missing but mandatory");
-        	}
-        }
-		
-	}
-	
-	
-	/**
-	 * Performs an index update of based on a ProcessNotification
-	 * 
+    @Override
+    public void validateProcessNotification(Header header, ProcessNotificationType request) {
+        validateEngagementTransactions(request.getEngagementTransaction(), true);
+    }
+
+
+    /**
+     * Performs an index update of based on a ProcessNotification
+     * 
      * @param header the header
      * @param request the request
      * @return the processNotifcation response
-	 */
-	@Override
-	@Transactional(isolation=Isolation.READ_UNCOMMITTED)
-	public ProcessNotificationResponseType processNotification(Header header, ProcessNotificationType request) {
-		LOG.debug("The svc.processNotification service is called");
+     */
+    @Override
+    @Transactional(isolation=Isolation.READ_UNCOMMITTED)
+    public ProcessNotificationResponseType processNotification(Header header, ProcessNotificationType request) {
+        LOG.debug("The svc.processNotification service is called");
 
-		// Separate deletes from the saves...
-		final List<EngagementTransactionType> engagementTransactions = request.getEngagementTransaction();
-		final List<Engagement> saveList = new ArrayList<Engagement>(engagementTransactions.size());
-		List<Engagement> deleteList = null;
-		for (final EngagementTransactionType engagementTransaction : engagementTransactions) {
+        // Separate deletes from the saves...
+        final List<EngagementTransactionType> engagementTransactions = request.getEngagementTransaction();
+        final List<Engagement> saveList = new ArrayList<Engagement>(engagementTransactions.size());
+        List<Engagement> deleteList = null;
+        for (final EngagementTransactionType engagementTransaction : engagementTransactions) {
 
-			EngagementType et = engagementTransaction.getEngagement();
+            EngagementType et = engagementTransaction.getEngagement();
 
-			final Engagement e = toEntity(et);	
-			if (engagementTransaction.isDeleteFlag()) {
-				if (deleteList == null) {
-					deleteList = new ArrayList<Engagement>();
-				}
-				deleteList.add(e);
-			} else {
-				saveList.add(e);
-			}
-		}
+            final Engagement e = toEntity(et);	
+            if (engagementTransaction.isDeleteFlag()) {
+                if (deleteList == null) {
+                    deleteList = new ArrayList<Engagement>();
+                }
+                deleteList.add(e);
+            } else {
+                saveList.add(e);
+            }
+        }
 
 
-		// R5 - fetch all posts that should be removed since the incoming Engagement changed owner
-		List<Engagement> engagementsWithNewOwners = getEngagementsWithNewOwners(request);
-		if(engagementsWithNewOwners.size() > 0) {
-			if(deleteList == null) {
-				deleteList = new ArrayList<Engagement>();
-			}
-			
-			for(final Engagement e : engagementsWithNewOwners) {
-				LOG.warn("The owner has changed for Engagement with ID: " +  e.getId());
-				deleteList.add(e);
-			}
-		}
-		
-		// Perform the delete if any
-		if (deleteList != null) {
-			engagementRepository.delete(deleteList);
-		}
+        // R5 - fetch all posts that should be removed since the incoming Engagement changed owner
+        List<Engagement> engagementsWithNewOwners = getEngagementsWithNewOwners(request);
+        if(engagementsWithNewOwners.size() > 0) {
+            if(deleteList == null) {
+                deleteList = new ArrayList<Engagement>();
+            }
 
-		// Perform the save
-		engagementRepository.save(saveList);
+            for(final Engagement e : engagementsWithNewOwners) {
+                LOG.warn("The owner has changed for Engagement with ID: " +  e.getId());
+                deleteList.add(e);
+            }
+        }
 
-		return NOTIFICATION_RESPONSE_OK;
-	}
+        // Perform the delete if any
+        if (deleteList != null) {
+            engagementRepository.delete(deleteList);
+        }
 
-	
-	/**
-	 * Removes all engagements that has this index as owner 
-	 * @param request 
-	 */
-	@Override
-	public ProcessNotificationType filterProcessNotification(ProcessNotificationType request) {
+        // Perform the save
+        engagementRepository.save(saveList);
 
-		final Iterator<EngagementTransactionType> iter = request.getEngagementTransaction().iterator();
-		while(iter.hasNext()) {
-			EngagementTransactionType e = iter.next();
-			if(e.getEngagement().getOwner().equals(this.owner)) {
-				iter.remove();
-			}
-		}
-		
-		return request;
-	}
-	
-	
-	/**
-	 * R5 - fetch all posts that should be removed since the incoming Engagement changed owner
-	 * 
-	 * This method is not part of the public API
-	 * TODO (patrik) - refactor the test so the this method can be private
-	 * 
-	 * @param request
-	 * @return List with engagements to remove
-	 */
-	public List<Engagement> getEngagementsWithNewOwners(ProcessNotificationType request) {
-		
-		final List<EngagementTransactionType> engagementTransactions = request.getEngagementTransaction();
-		final List<String> ids = new ArrayList<String>(engagementTransactions.size());
-		
-		for (final EngagementTransactionType engagementTransaction : engagementTransactions) {
-			EngagementType et = engagementTransaction.getEngagement();
-			ids.add(toEntity(et, this.owner).getId());
-		}
+        return NOTIFICATION_RESPONSE_OK;
+    }
 
-                // The request fail if findByIdIn receives an empty list
-		if (ids.size() == 0) {
-			return Collections.emptyList();
-		} else {
-			return engagementRepository.findByIdIn(ids);
-		}
-	}
-	
+
+    /**
+     * Removes all engagements that has this index as owner 
+     * @param request 
+     */
+    @Override
+    public ProcessNotificationType filterProcessNotification(ProcessNotificationType request) {
+
+        final Iterator<EngagementTransactionType> iter = request.getEngagementTransaction().iterator();
+        while(iter.hasNext()) {
+            EngagementTransactionType e = iter.next();
+            if(e.getEngagement().getOwner().equals(this.owner)) {
+                iter.remove();
+            }
+        }
+
+        return request;
+    }
+
+
+    /**
+     * R5 - fetch all posts that should be removed since the incoming Engagement changed owner
+     * 
+     * This method is not part of the public API
+     * TODO (patrik) - refactor the test so the this method can be private
+     * 
+     * @param request
+     * @return List with engagements to remove
+     */
+    public List<Engagement> getEngagementsWithNewOwners(ProcessNotificationType request) {
+
+        final List<EngagementTransactionType> engagementTransactions = request.getEngagementTransaction();
+        final List<String> ids = new ArrayList<String>(engagementTransactions.size());
+
+        for (final EngagementTransactionType engagementTransaction : engagementTransactions) {
+            EngagementType et = engagementTransaction.getEngagement();
+            ids.add(toEntity(et, this.owner).getId());
+        }
+
+        // The request fail if findByIdIn receives an empty list
+        if (ids.size() == 0) {
+            return Collections.emptyList();
+        } else {
+            return engagementRepository.findByIdIn(ids);
+        }
+    }
+
 
 }
