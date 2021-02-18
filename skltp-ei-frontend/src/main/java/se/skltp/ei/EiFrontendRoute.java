00dff6b13c523aed3971d60b02335884d8593d1a
WIKLUND Hans <hans.wiklund@soprasteria.com>
2021-02-18 15:39:23 +0100

    revert

--- skltp-ei-frontend/src/main/java/se/skltp/ei/EiFrontendRoute.java
+++ skltp-ei-frontend/src/main/java/se/skltp/ei/EiFrontendRoute.java
@@ -3,25 +3,7 @@ package se.skltp.ei;
 import static org.apache.camel.ExchangePattern.InOnly;
 import static se.skltp.ei.service.constants.EiConstants.EI_LOG_NUMBER_OF_RECORDS_IN_MESSAGE;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
-import javax.xml.soap.Detail;
-import javax.xml.soap.DetailEntry;
-import javax.xml.soap.MessageFactory;
-import javax.xml.soap.Name;
-import javax.xml.soap.SOAPBody;
-import javax.xml.soap.SOAPEnvelope;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.SOAPFault;
-import javax.xml.soap.SOAPMessage;
-
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-import org.apache.camel.builder.ErrorHandlerBuilder;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.cxf.binding.soap.SoapFault;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -29,14 +11,10 @@ import riv.itintegration.engagementindex.processnotification._1.rivtabp21.Proces
 import riv.itintegration.engagementindex.processnotification._1.rivtabp21.ProcessNotificationResponderService;
 import riv.itintegration.engagementindex.update._1.rivtabp21.UpdateResponderInterface;
 import riv.itintegration.engagementindex.update._1.rivtabp21.UpdateResponderService;
-import se.skltp.ei.errorhandling.ExceptionMessageProcessor;
 import se.skltp.ei.notification.CreateProcessNotificationResponseProcessor;
 import se.skltp.ei.notification.RemoveCircularProcessNotificationsProcessor;
 import se.skltp.ei.notification.ProcessNotificationRequestToJmsMsgProcessor;
 import se.skltp.ei.notification.ValidateProcessNotificationProcessor;
-import se.skltp.ei.service.api.EiException;
-import se.skltp.ei.service.util.ConvertRequestCharset;
-import se.skltp.ei.service.util.ConvertResponseCharset;
 import se.skltp.ei.update.SetOwnerProcessor;
 import se.skltp.ei.update.UpdateRequestToJmsMessageProcessor;
 import se.skltp.ei.update.CreateUpdateResponseProcessor;
@@ -78,9 +56,6 @@ public class EiFrontendRoute extends RouteBuilder {
   @Autowired
   RemoveCircularProcessNotificationsProcessor removeCircularProcessNotificationsProcessor;
 
-  @Autowired
-  ExceptionMessageProcessor exceptionMessageProcessor;
-  
   @Value("${update.collect.threshold:0}")
   int collectThreshold;
 
@@ -96,18 +71,7 @@ public class EiFrontendRoute extends RouteBuilder {
   @Value("${processnotification.webservice.url}")
   String notificationWebserviceUrl;
 
-  @Autowired
-  private ConvertRequestCharset convertRequestCharset;
 
-  @Autowired
-  private ConvertResponseCharset convertResponseCharset;
-  
-  public static final String LOG_ERROR_METHOD = "logError(*,${exception.stacktrace})";
-  public static final String LOG_RESP_OUT_METHOD = "logRespOut(*)";
-  public static final String LOG_REQ_IN_METHOD = "logReqIn(*)";
-  public static final String LOG_REQ_OUT_METHOD = "logReqOut(*)";
-  public static final String LOG_RESP_IN_METHOD = "logRespIn(*)";
-  
   @Override
   public void configure() throws Exception {
 
@@ -124,17 +88,8 @@ public class EiFrontendRoute extends RouteBuilder {
         , ProcessNotificationResponderInterface.class.getName()
         , ProcessNotificationResponderService.ProcessNotificationResponderPort.toString());
 
-    
-    //onException(EiException.class)
 
-    //.process(exceptionMessageProcessor);
-    // .bean(MessageInfoLogger.class, LOG_ERROR_METHOD)
-    // .process(convertResponseCharset)
-    // .removeHeaders(headerFilter.getResponseHeadersToRemove(), headerFilter.getResponseHeadersToKeep())
-    // .bean(MessageInfoLogger.class, LOG_RESP_OUT_METHOD)
-    // .handled(true);
-    
-    from(updateConfigurationPath)
+    from(updateConfigurationPath).streamCaching()
         .id("frontend-update-webservice-route")
         .log("Update SOAP call received")
         .process(validateUpdateProcessor)
@@ -147,7 +102,6 @@ public class EiFrontendRoute extends RouteBuilder {
             .to(InOnly,"activemq:queue:"+collectQueueName)
         .end()
         .process(createUpdateResponseProcessor);
-    	
 
     from(processNotificationConfigurationPath).streamCaching()
         .id("frontend-notification-webservice-route")
@@ -164,5 +118,6 @@ public class EiFrontendRoute extends RouteBuilder {
 
 
   }
+
 
 }
