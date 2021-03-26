553c6f9374e3dd8e6070364d6e527b228f416b6d
WIKLUND Hans <hans.wiklund@soprasteria.com>
2021-03-26 08:18:55 +0100

    configuration for amq

--- skltp-ei-frontend/src/main/java/se/skltp/ei/EiFrontendApplication.java
+++ skltp-ei-frontend/src/main/java/se/skltp/ei/EiFrontendApplication.java
@@ -2,8 +2,10 @@ package se.skltp.ei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { JmsAutoConfiguration.class, ActiveMQAutoConfiguration.class})
 public class EiFrontendApplication {
   public static void main(String[] args) {
     SpringApplication.run(EiFrontendApplication.class, args);
