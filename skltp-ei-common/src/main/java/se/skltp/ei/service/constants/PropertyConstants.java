4413da3ad83e25f1b168a6ec2b3fd213b0941bda
Christer Wendel <christer.wendel@soprasteria.com>
2021-04-08 14:18:07 +0200

    NTP-1911: Activate authentication for hawtio.
    * Move hawtio dependencies from skltp-ei-frontend and skltp-ei-backend to skltp-ei-common.
    * Copy custom hawtio configuration from AgP to skltp-ei-common.
    * Document the properties introduced.

---
+++ skltp-ei-common/src/main/java/se/skltp/ei/service/constants/PropertyConstants.java
@@ -0,0 +1,10 @@
+package se.skltp.ei.service.constants;
+
+public class PropertyConstants {
+
+  private PropertyConstants() {
+  }
+
+  public static final String HAWTIO_AUTHENTICATION_ENABLED = "hawtio.authentication.enabled";
+  public static final String HAWTIO_EXTERNAL_LOGINFILE = "hawtio.external.loginfile";
+}
