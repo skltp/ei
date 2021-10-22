c9d0717e3f91c7cbeecb1cc95ace805a5039f844
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2021-10-22 08:03:29 +0200

    NTP-2034 Get subscribers after reset

---
+++ skltp-ei-backend/src/test/java/se/skltp/ei/integrationtests/ResetSubscriberCacheIT.java
@@ -0,0 +1,38 @@
+package se.skltp.ei.integrationtests;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import se.skltp.ei.EiBackendApplication;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static se.skltp.ei.EiTeststubRoute.LOGICALADDREESS_MOCK;
+
+@CamelSpringBootTest
+@SpringBootTest(classes = {EiBackendApplication.class})
+@ActiveProfiles("teststub")
+public class ResetSubscriberCacheIT {
+  @Produce
+  protected ProducerTemplate producerTemplate;
+
+  @EndpointInject(LOGICALADDREESS_MOCK)
+  private MockEndpoint logicalAddresseesMock;
+
+  @Test
+  public void resetSubscriberCacheTest() throws InterruptedException {
+    logicalAddresseesMock.expectedMessageCount(1);
+    logicalAddresseesMock.assertIsSatisfied();
+
+    String resetResponse = producerTemplate.requestBody("{{subscriber.cache.reset.url}}", "", String.class);
+
+    assertTrue (resetResponse.contains("Subscriber cache reset"));
+    logicalAddresseesMock.expectedMessageCount(2);
+    logicalAddresseesMock.assertIsSatisfied();
+  }
+
+}
