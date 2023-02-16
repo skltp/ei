60888f8518390149f8e9245fa61a27a48cf4dd83
Björn Olsson Ågren <bjorn.olssonagren@nordicmedtest.se>
2023-02-16 13:33:11 +0100

    Minor adjustments to exponential backoff, and code formatting.

--- skltp-ei-backend/src/main/java/se/skltp/ei/route/EiBackendDynamicNotificationRoute.java
+++ skltp-ei-backend/src/main/java/se/skltp/ei/route/EiBackendDynamicNotificationRoute.java
@@ -3,15 +3,11 @@ package se.skltp.ei.route;
 import static se.skltp.ei.service.constants.EiConstants.X_VP_INSTANCE_ID;
 import static se.skltp.ei.service.constants.EiConstants.X_VP_SENDER_ID;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.DeadLetterChannelBuilder;
-import org.apache.camel.builder.ErrorHandlerBuilder;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spi.PropertiesComponent;
-import org.springframework.beans.factory.annotation.Value;
 
 import lombok.extern.log4j.Log4j2;
 import riv.itintegration.engagementindex.processnotification._1.rivtabp21.ProcessNotificationResponderInterface;
@@ -22,70 +18,72 @@ import se.skltp.ei.updateprocess.CreateProcessNotificationRequestProcessor;
 /**
  * Route definition for sending notifications to subscribers.
  * <br><br>
- * Queue name: EI.NOTIFICATION.subscriber 
+ * Queue name: EI.NOTIFICATION.subscriber
  * <br>where subscriber is a logical address.
- * 
+ *
  * <br><br>
- * The message is routed through vp (processnotification.serviceEndpointUrl) to the subscriber.
- * This requires that the X_VP_* headers are set to allow access to vp.
+ * The message is routed through vp (processnotification.serviceEndpointUrl) to
+ * the subscriber. This requires that the X_VP_* headers are set to allow access
+ * to vp.
  */
 @Log4j2
 public class EiBackendDynamicNotificationRoute extends RouteBuilder {
 
-  Subscriber subscriber;
-  
-  private int maximumRedeliveries = 0;
-  private int redeliveryDelay = 0;
-  private double backOffMultiplier = 0.0d;
-  private boolean useExponentialBackOff = false;
+    Subscriber subscriber;
 
-  public static final String PROCESSNOTIFICATION_WSDL="/schemas/TD_ENGAGEMENTINDEX_1_0_R/interactions/ProcessNotificationInteraction/ProcessNotificationInteraction_1.0_RIVTABP21.wsdl";
+    private int maximumRedeliveries = 0;
+    private int redeliveryDelay = 0;
+    private double backOffMultiplier = 0.0d;
+    private boolean useExponentialBackOff = false;
 
-  public EiBackendDynamicNotificationRoute(Subscriber subscriber, int maximumRedeliveries, int redeliveryDelay, double backOffMultiplier, boolean useExponentialBackOff) {
-	    this.subscriber = subscriber;
-	    this.maximumRedeliveries = maximumRedeliveries;
-	    this.redeliveryDelay = redeliveryDelay;
+    public static final String PROCESSNOTIFICATION_WSDL = "/schemas/TD_ENGAGEMENTINDEX_1_0_R/interactions/ProcessNotificationInteraction/ProcessNotificationInteraction_1.0_RIVTABP21.wsdl";
+
+    public EiBackendDynamicNotificationRoute(Subscriber subscriber, int maximumRedeliveries, int redeliveryDelay, double backOffMultiplier, boolean useExponentialBackOff) {
+        this.subscriber = subscriber;
+        this.maximumRedeliveries = maximumRedeliveries;
+        this.redeliveryDelay = redeliveryDelay;
         this.backOffMultiplier = backOffMultiplier;
         this.useExponentialBackOff = useExponentialBackOff;
-  }
-
-	private String getDeadLetterQueueName() {
-		  return "DLQ.".concat(subscriber.getNotificationQueueName());
-	  }
-  
-  @Override
-  public void configure() {
-
-    DeadLetterChannelBuilder builder = deadLetterChannel(String.format("activemq:queue:%s", getDeadLetterQueueName()));
-    builder.useOriginalMessage()
-           .maximumRedeliveries(maximumRedeliveries)
-           .redeliveryDelay(redeliveryDelay)
-           .onRedelivery(new Processor() {
-               @Override
-               public void process(Exchange exchange) throws Exception {
-                   log.error("Redelivery no "
-                   + exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER, Integer.class)
-                   + " from " + subscriber.getNotificationQueueName());
-               }
-           });
-    if(useExponentialBackOff) {
-        log.debug("Using exponential backoff for " + getDeadLetterQueueName() + " with backoff multiplier: " + backOffMultiplier);
-        builder.useExponentialBackOff()
-               .backOffMultiplier(backOffMultiplier);
     }
-    
- 	 errorHandler(builder);
- 	 
-	  fromF("activemq:queue:%s?transacted=true", subscriber.getNotificationQueueName())
-        .id(subscriber.getNotificationRouteName())
-        .log(LoggingLevel.DEBUG, "eiBackendLog","Got from notification queue")
-        .process(new CreateProcessNotificationRequestProcessor(subscriber.getLogicalAdress()))
-        .setHeader(X_VP_SENDER_ID, simple("{{processnotification.vpSenderId}}"))
-        .setHeader(X_VP_INSTANCE_ID, simple("{{processnotification.vpInstanceId}}"))
-        .toF("cxf:{{processnotification.serviceEndpointUrl}}?wsdlURL=%s&serviceClass=%s&portName=%s&cxfConfigurer=#eiBackendConfigBean"
-            , PROCESSNOTIFICATION_WSDL
-            , ProcessNotificationResponderInterface.class.getName()
-            , ProcessNotificationResponderService.ProcessNotificationResponderPort.toString());
-  }
+
+    private String getDeadLetterQueueName() {
+        return "DLQ.".concat(subscriber.getNotificationQueueName());
+    }
+
+    @Override
+    public void configure() {
+
+        DeadLetterChannelBuilder builder = deadLetterChannel(String.format("activemq:queue:%s", getDeadLetterQueueName()));
+
+        if (useExponentialBackOff) {
+            log.info("Using exponential backoff for " + getDeadLetterQueueName() + " with backoff multiplier: " + backOffMultiplier);
+            builder.useExponentialBackOff()
+                    .backOffMultiplier(backOffMultiplier);
+        }
+        builder.useOriginalMessage()
+                .maximumRedeliveries(maximumRedeliveries)
+                .redeliveryDelay(redeliveryDelay)
+                .onRedelivery(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        log.error("Redelivery no "
+                                + exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER, Integer.class)
+                                + " from " + subscriber.getNotificationQueueName());
+                    }
+                });
+
+        errorHandler(builder);
+
+        fromF("activemq:queue:%s?transacted=true", subscriber.getNotificationQueueName())
+                .id(subscriber.getNotificationRouteName())
+                .log(LoggingLevel.DEBUG, "eiBackendLog", "Got from notification queue")
+                .process(new CreateProcessNotificationRequestProcessor(subscriber.getLogicalAdress()))
+                .setHeader(X_VP_SENDER_ID, simple("{{processnotification.vpSenderId}}"))
+                .setHeader(X_VP_INSTANCE_ID, simple("{{processnotification.vpInstanceId}}"))
+                .toF("cxf:{{processnotification.serviceEndpointUrl}}?wsdlURL=%s&serviceClass=%s&portName=%s&cxfConfigurer=#eiBackendConfigBean",
+                        PROCESSNOTIFICATION_WSDL,
+                        ProcessNotificationResponderInterface.class.getName(),
+                        ProcessNotificationResponderService.ProcessNotificationResponderPort.toString());
+    }
 
 }
--- skltp-ei-backend/src/main/java/se/skltp/ei/route/EiBackendUpdateRoute.java
+++ skltp-ei-backend/src/main/java/se/skltp/ei/route/EiBackendUpdateRoute.java
@@ -14,63 +14,60 @@ import se.skltp.ei.updateprocess.UpdatePersistentStorageProcessor;
 @Component
 public class EiBackendUpdateRoute extends RouteBuilder {
 
+    @Value("${process.queue.name:process}")
+    String processQueueName;
 
-  @Value("${process.queue.name:process}")
-  String processQueueName;
+    @Autowired
+    UpdatePersistentStorageProcessor updatePersistentStorageProcessor;
 
-  @Autowired
-  UpdatePersistentStorageProcessor updatePersistentStorageProcessor;
+    @Autowired
+    NotificationSplitterBean notificationSplitterBean;
 
-  @Autowired
-  NotificationSplitterBean notificationSplitterBean;
+    @Value("${activemq.broker.maximum-redeliveries:0}")
+    private int maximumRedeliveries;
 
-  @Value("${activemq.broker.maximum-redeliveries:0}")
-  private int maximumRedeliveries;
+    @Value("${activemq.broker.redelivery-delay:0}")
+    private int redeliveryDelay;
 
-  @Value("${activemq.broker.redelivery-delay:0}")
-  private int redeliveryDelay;
-  
-  @Value("${activemq.broker.backoff-multiplier:0.0}")
-  private Double backOffMultiplier;
+    @Value("${activemq.broker.backoff-multiplier:0.0}")
+    private Double backOffMultiplier;
 
-  @Value("${activemq.broker.use-exponential-backoff:false}")
-  private Boolean useExponentialBackoff;
-  
-  @Override
-  public void configure() throws Exception {
+    @Value("${activemq.broker.use-exponential-backoff:false}")
+    private Boolean useExponentialBackoff;
 
-    DeadLetterChannelBuilder builder = deadLetterChannel(String.format("activemq:queue:DLQ.%s", processQueueName));
-    builder.useOriginalMessage()
-           .maximumRedeliveries(maximumRedeliveries)
-           .redeliveryDelay(redeliveryDelay)
-           .onRedelivery(new Processor() {
-                  @Override
-                  public void process(Exchange exchange) throws Exception {
-                      log.error("Redelivery no "
-                      + exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER, Integer.class)
-                      + " from " + processQueueName);
-                  }
-              });
-    if(useExponentialBackoff){
-        log.debug("Using exponential backoff for " + processQueueName + " with backoff multiplier: " + backOffMultiplier);
-        builder.useExponentialBackOff()
-               .backOffMultiplier(backOffMultiplier);
-    }
+    @Override
+    public void configure() throws Exception {
 
- 	 errorHandler(builder);
-     
-    // Get from process queue
-    fromF("activemq:queue:%s?transacted=true", processQueueName)
-        .id("backend-process-route")
-        .log(LoggingLevel.DEBUG, "eiBackendLog","Got one from Process Queue:\n${body}")
-        .process(updatePersistentStorageProcessor)
-        .split(method(notificationSplitterBean, "createNotificationList"))
-              .parallelProcessing(true)
-          .log(LoggingLevel.DEBUG, "eiBackendLog","Add notification to ${header.NotificationQueueName}")
-          .toD("activemq:queue:${header.NotificationQueueName}")
-        .end();
+        DeadLetterChannelBuilder builder = deadLetterChannel(String.format("activemq:queue:DLQ.%s", processQueueName));
+        if (useExponentialBackoff) {
+            log.info("Using exponential backoff for " + processQueueName + " with backoff multiplier: " + backOffMultiplier);
+            builder.useExponentialBackOff()
+                    .backOffMultiplier(backOffMultiplier);
+        }
+        builder.useOriginalMessage()
+                .maximumRedeliveries(maximumRedeliveries)
+                .redeliveryDelay(redeliveryDelay)
+                .onRedelivery(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        log.error("Redelivery no "
+                                + exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER, Integer.class)
+                                + " from " + processQueueName);
+                    }
+                });
 
+        errorHandler(builder);
 
+        // Get from process queue
+        fromF("activemq:queue:%s?transacted=true", processQueueName)
+                .id("backend-process-route")
+                .log(LoggingLevel.DEBUG, "eiBackendLog", "Got one from Process Queue:\n${body}")
+                .process(updatePersistentStorageProcessor)
+                .split(method(notificationSplitterBean, "createNotificationList"))
+                .parallelProcessing(true)
+                .log(LoggingLevel.DEBUG, "eiBackendLog", "Add notification to ${header.NotificationQueueName}")
+                .toD("activemq:queue:${header.NotificationQueueName}")
+                .end();
 
     }
 
