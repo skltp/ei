f695e1ed3df6eb822aeb71ffdbca580bd6602b1c
Henrik Augustsson <henrik.augustsson@nordicmedtest.se>
2025-02-04 14:14:24 +0100

    Todo comment update to reflect current state.

--- skltp-ei-backend/src/main/java/se/skltp/ei/EiBackendApplication.java
+++ skltp-ei-backend/src/main/java/se/skltp/ei/EiBackendApplication.java
@@ -9,12 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
-// TODO: Spring Boot 3.1.4 and 3.4.x reintroduces the AutoConfiguration for ActiveMQ.
+// TODO: Spring Boot 3.4.x will reintroduce some AutoConfiguration for ActiveMQ.
 //    v3.1.4+ and 3.2 had a version that allowed partial AutoConfig, but excluded broker functions,
 //       as that hadn't been updated to jakarta in some dependencies. I.o.w: a partial compatibility with old v5.8 AMQ logic.
 //    v3.3 saw a full removal of AutoConfig as AMQ moved to v6, and dropped old support.
 //    v3.4 will see the reintroduction of AMQ AutoConfig support for v6.
-//    Might need to be excluded again, or reworked to depend on properties instead of manual setup.
+//    Might need to be excluded or reworked to depend on properties instead of manual setup.
 
 import org.springframework.context.ConfigurableApplicationContext;
 
